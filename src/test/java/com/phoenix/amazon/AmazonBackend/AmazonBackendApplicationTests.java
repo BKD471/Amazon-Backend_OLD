789b2fd8e480eb667c0ccd78a398c7e8eb7a1ec7
BKD471 <bhaskarkumardas9@gmail.com>
2024-01-13 21:30:12 +0530

    ssetup

---
+++ src/test/java/com/phoenix/amazon/AmazonBackend/AmazonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.amazon.AmazonBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
