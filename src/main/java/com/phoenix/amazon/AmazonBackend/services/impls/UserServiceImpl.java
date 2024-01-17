644f4269a2b04431eb0992081a3546d1066c4078
BKD471 <bhaskarkumardas9@gmail.com>
2024-01-18 01:05:40 +0530

    setting up user creation service

--- src/main/java/com/phoenix/amazon/AmazonBackend/services/impls/UserServiceImpl.java
+++ src/main/java/com/phoenix/amazon/AmazonBackend/services/impls/UserServiceImpl.java
@@ -1,12 +1,20 @@
 package com.phoenix.amazon.AmazonBackend.services.impls;
 
 import com.phoenix.amazon.AmazonBackend.dto.UserDto;
+import com.phoenix.amazon.AmazonBackend.entity.Users;
+import com.phoenix.amazon.AmazonBackend.helpers.AllConstantHelpers;
 import com.phoenix.amazon.AmazonBackend.repository.IUserRepository;
 import com.phoenix.amazon.AmazonBackend.services.IUserService;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.UUID;
 
-import static  com.phoenix.amazon.AmazonBackend.helpers.AllConstantHelpers.UserFields;
+import static com.phoenix.amazon.AmazonBackend.helpers.AllConstantHelpers.UserFields;
+import static com.phoenix.amazon.AmazonBackend.helpers.MappingHelpers.UserDtoToUsers;
+import static com.phoenix.amazon.AmazonBackend.helpers.MappingHelpers.UsersToUsersDto;
 
+@Service("UserServiceMain")
 public class UserServiceImpl implements IUserService {
     private final IUserRepository userRepository;
 
@@ -14,13 +22,29 @@ public class UserServiceImpl implements IUserService {
         this.userRepository = userRepository;
     }
 
+    private UserDto initializeUserId(final UserDto userDto){
+        final String userIdUUID=UUID.randomUUID().toString();
+        return new UserDto.builder()
+                .userId(userIdUUID)
+                .name(userDto.name())
+                .email(userDto.email())
+                .gender(userDto.gender())
+                .imageName(userDto.imageName())
+                .password(userDto.password())
+                .about(userDto.about())
+                .build();
+    }
     /**
-     * @param user
+     * @param userDto
      * @return
      */
     @Override
-    public UserDto createUser(UserDto user) {
-        return null;
+    public UserDto createUser(final UserDto userDto) {
+        UserDto userDtoWithId=initializeUserId(userDto);
+        Users user = UserDtoToUsers(userDtoWithId);
+
+        Users savedUser = userRepository.save(user);
+        return UsersToUsersDto(savedUser);
     }
 
     /**
