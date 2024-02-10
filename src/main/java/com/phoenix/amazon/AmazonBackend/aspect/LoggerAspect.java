b58d9c5c0c041ab3f853806ef9891112a281e6e1
BKD471 <bhaskarkumardas9@gmail.com>
2024-02-11 02:35:15 +0530

    added loggers

--- src/main/java/com/phoenix/amazon/AmazonBackend/aspect/LoggerAspect.java
+++ src/main/java/com/phoenix/amazon/AmazonBackend/aspect/LoggerAspect.java
@@ -1,5 +1,6 @@
 package com.phoenix.amazon.AmazonBackend.aspect;
 
+import com.phoenix.amazon.AmazonBackend.dto.PageableResponse;
 import com.phoenix.amazon.AmazonBackend.dto.UserDto;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -8,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.InputStream;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
@@ -20,7 +22,7 @@ public class LoggerAspect {
     Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
 
     @Around(value = "execution(com.phoenix.amazon.AmazonBackend.dto.UserDto   com.phoenix.amazon.AmazonBackend.services.*.*(..))")
-    public UserDto log(final ProceedingJoinPoint joinPoint) throws Throwable {
+    public UserDto logUserDtoType(final ProceedingJoinPoint joinPoint) throws Throwable {
 
         LocalTime localTime = LocalTime.now(Clock.system(ZoneId.of("Asia/Kolkata")));
         logger.info(String.format("<################# %s   ", localTime) + joinPoint.
@@ -32,13 +34,115 @@ public class LoggerAspect {
         Instant end = Instant.now();
         long timeElapsedInMs = Duration.between(start, end).toMillis();
         logger.info(String.format("<################# Time elapsed to execute %s is %s Ms ##############################" +
-                        "##########################################################################################>",
+                        "######################################################################################" +
+                        "#########>",
                 joinPoint.getSignature().toString(), timeElapsedInMs));
 
         localTime = LocalTime.now(Clock.system(ZoneId.of("Asia/Kolkata")));
         logger.info(String.format("<################# %s    ", localTime) + joinPoint.getSignature().toString() +
                 " method execution " +
-                "completed ############################################################################################>");
+                "completed #############################################################################################" +
+                "#################>");
         return (UserDto) result;
+    }
+
+    @Around(value = "execution(String   com.phoenix.amazon.AmazonBackend.services.*.*(..))")
+    public String logStringType(final ProceedingJoinPoint joinPoint) throws Throwable {
+
+        LocalTime localTime = LocalTime.now(Clock.system(ZoneId.of("Asia/Kolkata")));
+        logger.info(String.format("<################# %s   ", localTime) + joinPoint.
+                getSignature().
+                toString() + " method executions starts ################################################################" +
+                "#################################################>");
+        Instant start = Instant.now();
+        Object result = joinPoint.proceed();
+        Instant end = Instant.now();
+        long timeElapsedInMs = Duration.between(start, end).toMillis();
+        logger.info(String.format("<################# Time elapsed to execute %s is %s Ms ##############################" +
+                        "######################################################################################" +
+                        "#########>",
+                joinPoint.getSignature().toString(), timeElapsedInMs));
+
+        localTime = LocalTime.now(Clock.system(ZoneId.of("Asia/Kolkata")));
+        logger.info(String.format("<################# %s    ", localTime) + joinPoint.getSignature().toString() +
+                " method execution " +
+                "completed #############################################################################################" +
+                "#################>");
+        return (String) result;
+    }
+
+    @Around(value = "execution(com.phoenix.amazon.AmazonBackend.dto.PageableResponse   com.phoenix.amazon.AmazonBackend.services.*.*(..))")
+    public PageableResponse<UserDto> logPageableResponseType(final ProceedingJoinPoint joinPoint) throws Throwable {
+
+        LocalTime localTime = LocalTime.now(Clock.system(ZoneId.of("Asia/Kolkata")));
+        logger.info(String.format("<################# %s   ", localTime) + joinPoint.
+                getSignature().
+                toString() + " method executions starts ################################################################" +
+                "#################################################>");
+        Instant start = Instant.now();
+        Object result = joinPoint.proceed();
+        Instant end = Instant.now();
+        long timeElapsedInMs = Duration.between(start, end).toMillis();
+        logger.info(String.format("<################# Time elapsed to execute %s is %s Ms ##############################" +
+                        "######################################################################################" +
+                        "#########>",
+                joinPoint.getSignature().toString(), timeElapsedInMs));
+
+        localTime = LocalTime.now(Clock.system(ZoneId.of("Asia/Kolkata")));
+        logger.info(String.format("<################# %s    ", localTime) + joinPoint.getSignature().toString() +
+                " method execution " +
+                "completed #############################################################################################" +
+                "#################>");
+        return (PageableResponse<UserDto>) result;
+    }
+
+    @Around(value = "execution(*   com.phoenix.amazon.AmazonBackend.services.*.*(..))")
+    public Object logObjectType(final ProceedingJoinPoint joinPoint) throws Throwable {
+
+        LocalTime localTime = LocalTime.now(Clock.system(ZoneId.of("Asia/Kolkata")));
+        logger.info(String.format("<################# %s   ", localTime) + joinPoint.
+                getSignature().
+                toString() + " method executions starts ################################################################" +
+                "#################################################>");
+        Instant start = Instant.now();
+        Object result = joinPoint.proceed();
+        Instant end = Instant.now();
+        long timeElapsedInMs = Duration.between(start, end).toMillis();
+        logger.info(String.format("<################# Time elapsed to execute %s is %s Ms ##############################" +
+                        "######################################################################################" +
+                        "#########>",
+                joinPoint.getSignature().toString(), timeElapsedInMs));
+
+        localTime = LocalTime.now(Clock.system(ZoneId.of("Asia/Kolkata")));
+        logger.info(String.format("<################# %s    ", localTime) + joinPoint.getSignature().toString() +
+                " method execution " +
+                "completed #############################################################################################" +
+                "#################>");
+        return result;
+    }
+
+    @Around(value = "execution(java.io.InputStream  com.phoenix.amazon.AmazonBackend.services.*.*(..))")
+    public InputStream logInputStreamType(final ProceedingJoinPoint joinPoint) throws Throwable {
+
+        LocalTime localTime = LocalTime.now(Clock.system(ZoneId.of("Asia/Kolkata")));
+        logger.info(String.format("<################# %s   ", localTime) + joinPoint.
+                getSignature().
+                toString() + " method executions starts ################################################################" +
+                "#################################################>");
+        Instant start = Instant.now();
+        Object result = joinPoint.proceed();
+        Instant end = Instant.now();
+        long timeElapsedInMs = Duration.between(start, end).toMillis();
+        logger.info(String.format("<################# Time elapsed to execute %s is %s Ms ##############################" +
+                        "######################################################################################" +
+                        "#########>",
+                joinPoint.getSignature().toString(), timeElapsedInMs));
+
+        localTime = LocalTime.now(Clock.system(ZoneId.of("Asia/Kolkata")));
+        logger.info(String.format("<################# %s    ", localTime) + joinPoint.getSignature().toString() +
+                " method execution " +
+                "completed #############################################################################################" +
+                "#################>");
+        return (InputStream) result;
     }
 }
